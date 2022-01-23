43ccf436fca7327997d7b13ce6dfebac7be236ec
Killian Chamberlain <killian7195@gmail.com>
2022-01-23 15:28:01 -0600

    big fix

--- public/js/idb.js
+++ public/js/idb.js
@@ -2,7 +2,7 @@
 let db;
 
 // establish a connection to IndexedDB database called 'budget_tracker' and set it to version 1
-const request = indexedDB.open("moneytracker_budget", 1);
+const request = indexedDB.open("budget_tracker", 1);
 
 request.onupgradeneeded = function (event) {
   // save a reference to the database
